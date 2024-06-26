b5c65f206d0bae184ab5d047b371572edf291284
Felipe Oliveira <felipe.n.oliveira@outlook.com>
2024-06-25 23:45:13 -0300

    FEATURE: Operadores Logicos, Operadores de Comparacao, Operadores de Igualdade e Atribuicao

--- FundamentoCsharp.Console/FundamentoCsharp.Console/Program.cs
+++ FundamentoCsharp.Console/FundamentoCsharp.Console/Program.cs
@@ -19,6 +19,8 @@ string valorString = "Felipe Neres";
 
 const double PI = 3.1416; // É uma constante não muda a partir do momento que foi atribuida
 
+Console.WriteLine();
+Console.WriteLine("Tipos e Variáveis");
 Console.WriteLine($"Double: {valorDouble}, Float: {valorFloat}, Decimal: {valorDecimal}");
 Console.WriteLine($"Int: {valorInteiro}, Boolean: {valorBoolean}");
 Console.WriteLine($"Char: {valorChar}, String: {valorString}");
@@ -37,7 +39,74 @@ int multiplicacao = a * b;
 int divisao = b / a;
 int modulo = a % b;
 
+Console.WriteLine();
+Console.WriteLine("Operadores Artiméticos");
 Console.WriteLine($"soma: {soma}, Subtracao: {subtracao}, Multiplicação: {multiplicacao}");
 Console.WriteLine($"Divisão: {divisao}, Modulo: {modulo}");
+
+#endregion
+
+#region Operadores Lógicos
+
+bool x = true;
+bool y = false;
+
+var and = x && y; // AND
+var or = x || y; // OR
+var notX = !x; //NOT
+var notY = !y; //NOT
+
+Console.WriteLine();
+Console.WriteLine("Operadores Lógicos");
+Console.WriteLine("x = true, y = false");
+Console.WriteLine($"AND: {and}, OR: {or}, NOT X: {notX}, NOT Y: {notY}");
+
+#endregion
+
+#region Operadores de Comparação
+
+var p = 5;
+var q = 10;
+
+bool maior = p > q;
+bool maiorOuIgual = p >= q;
+bool menor = p < q;
+bool menorOuIgual = p <= q;
+bool igual = p == q;
+bool diferente = p != q;
+
+Console.WriteLine();
+Console.WriteLine("Operadores de Comparação");
+Console.WriteLine($"Maior: {maior}, Maior ou Igual: {maiorOuIgual},");
+Console.WriteLine($"Menor: {menor}, Menor ou Igual: {menorOuIgual}," +
+    $" Igual: {igual}, Diferente: {diferente}");
+
+#endregion
+
+
+#region Operadores de Igualdade e Atribuição
+
+int k = 5;
+
+Console.WriteLine();
+Console.WriteLine("Operadores de Igualdade e Atribuição");
+
+k = 10;
+Console.WriteLine(k);
+
+k += 5; // k = k + 5 = 15 ou 10 + 5 = 15
+Console.WriteLine(k);
+
+k -= 3; // K = K - 3 = 12 ou 15 - 3 = 12
+Console.WriteLine(k);
+
+k *= 2; // k = k * 2 = 24 ou 12 * 2 = 24 
+Console.WriteLine(k);
+
+k /= 4; // k = k / 4 = 6 ou 24 / 4 = 6 
+Console.WriteLine(k);
+
+k %= 5; // k = k % 5 = 1 ou 6 % 5 = 1
+Console.WriteLine(k);
 
 #endregion
